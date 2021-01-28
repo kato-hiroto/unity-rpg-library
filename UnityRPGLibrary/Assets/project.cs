9bb7db69db01ac47cbad50163a891e10f918a2b8
iyoka <kato-hiroto@ed.tmu.ac.jp>
2021-01-28 15:59:42 +0900

    初期状態

---
+++ UnityRPGLibrary/Assets/project.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class project : MonoBehaviour
+{
+    // 必要な機能
+    // プレイヤー: 移動
+    // 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

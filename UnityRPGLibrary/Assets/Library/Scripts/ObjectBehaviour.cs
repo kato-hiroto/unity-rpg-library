e6ac19501707459a107bd2a71791b9f7abaec62b
iyoka <kato-hiroto@ed.tmu.ac.jp>
2021-02-01 10:22:01 +0900

    ObjectBehaviour作成開始

---
+++ UnityRPGLibrary/Assets/Library/Scripts/ObjectBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBehaviour : MonoBehaviour
+{
+
+    // 状態に関するメソッドを定義
+    // 1. 状態変数をグローバルリストに登録
+    // 2. 任意の状態変数を更新
+    // 3. 任意の状態変数の更新通知受け取り (コールバック関数)
+    // 4. すべての状態変数のファイル書き出し
+    // 5. ファイルからすべての状態変数読み込み
+    // 6. 自分の状態変数の更新通知を受け取って自身の状態を変化
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
